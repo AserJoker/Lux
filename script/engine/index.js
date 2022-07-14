83ed04bc940ff6bac77ab196f51491362a8dabac
w30029682 <wangjinfan1@huawei.com>
2022-07-14 09:43:02 +0800

    #runtime build

--- script/engine/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.start = void 0;
-var system_1 = require("./system");
-require("./scene");
-var start = function () {
-    system_1.System_Main.get();
-};
-exports.start = start;
