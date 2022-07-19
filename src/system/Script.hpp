e259122862e256fdf8a6e98313af8955b5efb81d
w30029682 <wangjinfan1@huawei.com>
2022-07-19 20:28:18 +0800

    #lua support

--- src/system/Script.hpp
+++ src/system/Script.hpp
@@ -1,12 +1,15 @@
 #ifndef _H_LUX_SYSTEM_SCRIPT_
 #define _H_LUX_SYSTEM_SCRIPT_
-#include<lua.h>
+#include<lua.hpp>
 #include "interface/IScript.hpp"
 namespace lux::system
 {
     class Script:public IScript{
         private:
-        
+            lua_State *_pState;
+        public:
+            Script():_pState(0){
+            };
     };
 } // namespace lux::system
 
